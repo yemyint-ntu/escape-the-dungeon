f57feb8becb892cd80866ed7f2a886ab2b806f00
kreate-lab <kreate.lab.mm@gmail.com>
2025-05-07 22:04:15 +0800

    Lesson 1 Instructions

---
+++ main.py
@@ -0,0 +1,20 @@
+# accept input from the player using the following prompt:
+# "What is your name, brave adventurer? " 
+# Store the user's name in a variable called character_name
+
+
+# output the following to the screen
+# Welcome to Escape the Dungeon, adventurer $character_name
+
+
+# output the following to the screen
+# Choose your class:
+# 1. Warrior (High Strength)
+# 2. Rogue (High Agility)
+# 3. Mage (High Magic)
+
+
+# accept input from the player using the following prompt
+# "Enter 1, 2, or 3: "
+# Store the user's choice in a variable called class_choice
+
